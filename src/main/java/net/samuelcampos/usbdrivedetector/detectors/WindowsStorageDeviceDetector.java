5043122e046188e3b62bd59938a14230cf65e527
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2018-07-24 20:14:51 +0200

    making possible to discover also usb disk drives on Windows

--- src/main/java/net/samuelcampos/usbdrivedetector/detectors/WindowsStorageDeviceDetector.java
+++ src/main/java/net/samuelcampos/usbdrivedetector/detectors/WindowsStorageDeviceDetector.java
@@ -1,30 +1,29 @@
 /*
  * Copyright 2014 samuelcampos.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package net.samuelcampos.usbdrivedetector.detectors;
 
-import net.samuelcampos.usbdrivedetector.USBStorageDevice;
-import net.samuelcampos.usbdrivedetector.process.CommandExecutor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.filechooser.FileSystemView;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.samuelcampos.usbdrivedetector.USBStorageDevice;
+import net.samuelcampos.usbdrivedetector.process.CommandExecutor;
 
 /**
  *
@@ -38,6 +37,7 @@ public class WindowsStorageDeviceDetector extends AbstractStorageDeviceDetector 
      * wmic logicaldisk where drivetype=2 get description,deviceid,volumename
      */
     private static final String CMD_WMI_USB = "wmic logicaldisk where drivetype=2 get deviceid";
+    private static final String CMD_WMI_DRIVES = "wmic logicaldisk where drivetype=3 get deviceid";
 
     protected WindowsStorageDeviceDetector() {
         super();
@@ -47,7 +47,14 @@ public class WindowsStorageDeviceDetector extends AbstractStorageDeviceDetector 
     public List<USBStorageDevice> getStorageDevicesDevices() {
         final ArrayList<USBStorageDevice> listDevices = new ArrayList<>();
 
-        try (CommandExecutor commandExecutor = new CommandExecutor(CMD_WMI_USB)) {
+        getDevices(listDevices, CMD_WMI_USB);
+        getDevices(listDevices, CMD_WMI_DRIVES);
+
+        return listDevices;
+    }
+
+    private void getDevices(final ArrayList<USBStorageDevice> listDevices, String command) {
+        try (CommandExecutor commandExecutor = new CommandExecutor(command)) {
             commandExecutor.processOutput(outputLine -> {
                 if (!outputLine.isEmpty() && !"DeviceID".equals(outputLine)) {
                     final String rootPath = outputLine + File.separatorChar;
@@ -58,8 +65,6 @@ public class WindowsStorageDeviceDetector extends AbstractStorageDeviceDetector 
         } catch (IOException e) {
             logger.error(e.getMessage(), e);
         }
-
-        return listDevices;
     }
 
     private String getDeviceName(final String rootPath) {
@@ -81,57 +86,4 @@ public class WindowsStorageDeviceDetector extends AbstractStorageDeviceDetector 
         return name;
     }
 
-    /**
-     * Returns the list of the removable devices actually connected to the computer. <br/>
-     * This method was effectively tested on:
-     * <ul>
-     * <li>Windows 7 (English)</li>
-     * </ul>
-     *
-     * @deprecated replaced by {@link #getWindowsRemovableDevicesCommand()}
-     *
-     * @return the list of removable devices
-     */
-//    @SuppressWarnings("unused")
-//    private ArrayList<USBStorageDevice> getWindowsRemovableDevicesList() {
-//
-//        /**
-//         * TODO: How to put this working in all languages?
-//         */
-//        String fileSystemDesc = "Removable Disk";
-//
-//        ArrayList<USBStorageDevice> listDevices = new ArrayList<USBStorageDevice>();
-//
-//        File[] roots = File.listRoots();
-//
-//        if (roots == null) {
-//            // TODO: raise an error?
-//            return listDevices;
-//        }
-//
-//        for (File root : roots) {
-//            if (root.canRead() && root.canWrite() && fsView.isDrive(root)
-//                    && !fsView.isFloppyDrive(root)) {
-//
-//                if (fileSystemDesc.equalsIgnoreCase(fsView
-//                        .getSystemTypeDescription(root))) {
-//                    USBStorageDevice device = new USBStorageDevice(root,
-//                            fsView.getSystemDisplayName(root));
-//                    listDevices.add(device);
-//                }
-//
-//                System.out.println(fsView.getSystemDisplayName(root) + " - "
-//                        + fsView.getSystemTypeDescription(root));
-//
-//                /*
-//                 * FileSystemView.getSystemTypeDescription();
-//                 * 
-//                 * Windows (8): Windows (7): "Removable Disk" Windows (XP):
-//                 * Linux (Ubuntu): OSX (10.7):
-//                 */
-//            }
-//        }
-//
-//        return listDevices;
-//    }
 }
